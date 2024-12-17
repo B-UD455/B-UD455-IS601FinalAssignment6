c7af37307f74aead5e65252219e9a5129470bbd9
B-UD455 <brandon.munroe@ndus.edu>
2024-12-17 12:12:48 -0500

    Update token_schema for comprehension

--- app/schemas/token_schema.py
+++ app/schemas/token_schema.py
@@ -1,3 +1,4 @@
+'''
 from builtins import str
 from pydantic import BaseModel
 
@@ -9,6 +10,22 @@ class TokenResponse(BaseModel):
         json_schema_extra = {
             "example": {
                 "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJqb2huZG9lQGV4YW1wbGUuY29tIiwicm9sZSI6IkFVVEhFTlRJQ0FURUQiLCJleHAiOjE2MjQzMzQ5ODR9.ZGNjNjI2ZjI4MmYzNTk0MjVjNDk0ZjI4MjdjNGEzNmI1",
+                "token_type": "bearer"
+            }
+        }
+'''
+
+from builtins import str
+from pydantic import BaseModel
+
+class TokenResponse(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJqb2huZG9lQGV4YW1wbGUuY29tIiwicm9sZSI6IkFVVEhFTlRJQ0FURUQiLCJleHAiOjE2MjQzMzQ5ODR9.ZGNjNjI2ZjI4MmYzNTk0MjVjNDk0ZjI4MjdjNGEzNmI1",  # Example token
                 "token_type": "bearer"
             }
         }
